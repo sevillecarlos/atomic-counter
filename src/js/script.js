9a9ef33e978f9d4a067cae624457117b548035c3
sevillecarlos <sevillecarlos@gmail.com>
2020-05-04 22:26:01 -0600

    init project setup

---
+++ src/js/script.js
@@ -0,0 +1,6 @@
+const inputFormula = document.querySelector(".formula-intro");
+const btnCalc = document.querySelector(".calc-btn");
+
+const showCalcContainer = document.querySelector(".show-calc");
+
+console.log('asidhas')
